ece9e763ea95f61cace75c4354e5d67bdf7ac715
ivaaner <ivan.ernandorena@leapsight.com>
2020-04-13 16:37:11 -0300

    first commit

---
+++ practicas/lib/enum_operations.ex
@@ -0,0 +1,18 @@
+defmodule EnumOperations do
+  @moduledoc """
+  Documentation for `EnumOperations`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EnumOperations.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
