7bd017c3a81404428390fd76ebb544d95e1c2d1d
ivaaner <ivan.ernandorena@leapsight.com>
2020-04-20 11:37:15 -0300

    deleted

--- lib/app_web/controllers/hello_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule AppWeb.HelloController do
-  use AppWeb, :controller
-
-  def index(conn, _params) do
-    render(conn, "index.html")
-  end
-end
--- lib/app_web/views/hello_view.ex
+++
@@ -1,3 +0,0 @@
-defmodule AppWeb.HelloView do
-  use AppWeb, :view
-end
